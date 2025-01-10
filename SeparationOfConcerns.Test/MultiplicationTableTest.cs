155c08cc5f6fb4e534bee6c8963525ccde20385f
markostojkovic00 <marko_stojkovic@sluz.ch>
2025-01-10 09:30:08 +0100

    First Test

--- SeparationOfConcerns.Test/MultiplicationTableTest.cs
+++ SeparationOfConcerns.Test/MultiplicationTableTest.cs
@@ -5,5 +5,16 @@ public class MultiplicationTableTest
     [Fact]
     public void Test()
     {
+        
+    }
+    
+    [Fact]
+    public void Test_BiggestValue()
+    {
+        var numbers = new List<int> { 3, 7, 2, 10, 5 };
+
+        var result = MultiplicationTable.findBiggestValue(numbers);
+
+        Assert.Equal(10, result);
     }
 }
